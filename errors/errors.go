26639b48eada1ba12887a3f928acdfe0575578c1
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-20 12:40:43 +0000

    refactor and cleanup errors

--- errors/errors.go
+++ errors/errors.go
@@ -2,41 +2,81 @@ package errors
 
 import "net/http"
 
-// ResError struct
-type ResError struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-	Error   string `json:"error"`
-	Causes []interface{} `json:"causes"`
+// ResError interface
+type ResError interface {
+	Error() string
+	Message() string
+	Status() int
+	Causes() []interface{}
+}
+
+// resError struct
+type resError struct {
+	message string        `json:"message"`
+	status  int           `json:"status"`
+	error   string        `json:"error"`
+	causes  []interface{} `json:"causes"`
+}
+
+func (re resError) Error() string {
+	return re.error
+}
+func (re resError) Message() string {
+	return re.message
+}
+func (re resError) Status() int {
+	return re.status
+}
+func (re resError) Causes() []interface{} {
+	return re.causes
 }
 
 // HandlerBadRequest func for ResError struct
-func HandlerBadRequest(message string) *ResError {
-	return &ResError{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func HandlerBadRequest(message string) ResError {
+	return resError{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "bad_request",
 	}
 }
 
 // HandlerNotFoundError func for ResError struct
-func HandlerNotFoundError(message string) *ResError {
-	return &ResError{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
+func HandlerNotFoundError(message string) ResError {
+	return resError{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   "not_found",
+	}
+}
+
+// HandlerUnauthorizedError func
+func HandlerUnauthorizedError(message string) ResError {
+	return resError{
+		message: message,
+		status:  http.StatusUnauthorized,
+		error:   "unauthorized",
 	}
 }
 
 // HandlerInternalServerError func for ResError struct
-func HandlerInternalServerError(message string, err error) *ResError {
-	ResError := &ResError{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
+func HandlerInternalServerError(message string, err error) ResError {
+	result := resError{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "internal_server_error",
 	}
 	if err != nil {
-		ResError.Causes = append(ResError.Causes, err.Error())
+		result.causes = append(result.causes, err.Error())
 	}
-	return ResError
+	return result
+}
+
+// HandleRestError func
+func HandleRestError(message string, status int, er string, causes []interface{}) ResError {
+	return resError{
+		message: message,
+		status:  http.StatusUnauthorized,
+		error:   er,
+		causes:  causes,
+	}
 }
